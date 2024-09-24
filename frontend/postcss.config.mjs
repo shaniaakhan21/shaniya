a0fa3d4b7862a5dfc34176494be70204d9b5b6b8
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2024-09-24 19:42:51 +0400

    postcss

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,11 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-const postcssConfig = {
-  plugins: [
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer,
-  ],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
 };
-
-export default postcssConfig;
+export default config;
