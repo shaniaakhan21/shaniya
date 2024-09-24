3e98c5e92f3ff6cd4783fa3253a4f66261e25b2d
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2024-09-24 20:50:55 +0400

    mext cofig

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Allow connections from all network interfaces
-    experimental: {
-      // outputFileTracing: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  // Allow connections from all network interfaces
+  experimental: {
+    // outputFileTracing: true,
+  },
+  async redirects() {
+      return [
+          {
+              source: '/:path*',
+              has: [
+                  {
+                      type: 'host',
+                      value: 'shaniya.pro', // Without www
+                  },
+              ],
+              destination: 'https://www.shaniya.pro/:path*', // Redirect to the www version
+              permanent: true,
+          },
+          {
+              source: '/:path*',
+              has: [
+                  {
+                      type: 'host',
+                      value: 'www.shaniya.pro', // With www
+                  },
+              ],
+              destination: 'https://www.shaniya.pro/:path*', // Ensure www also redirects to HTTPS
+              permanent: true,
+          },
+      ];
+  },
+};
+
+export default nextConfig;
