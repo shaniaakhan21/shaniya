b5d14a8f04954e0b1ce2194b8192690ef915e8a4
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2024-09-25 12:52:52 +0400

    next update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,34 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Allow connections from all network interfaces
-  experimental: {
-    // outputFileTracing: true,
-  },
   async redirects() {
-      return [
+    return [
+      {
+        source: '/:path*',
+        has: [
           {
-              source: '/:path*',
-              has: [
-                  {
-                      type: 'host',
-                      value: 'shaniya.pro', // Without www
-                  },
-              ],
-              destination: 'https://www.shaniya.pro/:path*', // Redirect to the www version
-              permanent: true,
+            type: 'host',
+            value: 'shaniya.pro', 
           },
+        ],
+        destination: 'https://www.shaniya.pro/:path*', 
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
           {
-              source: '/:path*',
-              has: [
-                  {
-                      type: 'host',
-                      value: 'www.shaniya.pro', // With www
-                  },
-              ],
-              destination: 'https://www.shaniya.pro/:path*', // Ensure www also redirects to HTTPS
-              permanent: true,
+            type: 'host',
+            value: 'www.shaniya.pro', 
           },
-      ];
+        ],
+        destination: 'https://www.shaniya.pro/:path*', 
+        permanent: true,
+      },
+    ];
   },
 };
 
