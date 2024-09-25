a9506d23b023e45634c57726f936f0c68e239958
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2024-09-25 13:03:09 +0400

    next update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,30 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'shaniya.pro', 
-          },
-        ],
-        destination: 'https://www.shaniya.pro/:path*', 
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.shaniya.pro', 
-          },
-        ],
-        destination: 'https://www.shaniya.pro/:path*', 
-        permanent: true,
-      },
-    ];
+  experimental: {
+    // outputFileTracing: true,
   },
 };
 
